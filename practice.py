8031122939ff9df172e1532c86e5c5089ef53350
Arun Balakrishnan <arun.balakrishnan@sti-aromas.com>
2026-03-14 17:28:01 +0530

    v 1.60 sliding window

--- practice.py
+++ practice.py
@@ -1,26 +1,7 @@
-def my_function(nums):
-    left = 0
-    right = len(nums) -1
-    max_area = 0
+def my_funtion(nums, k):
+    seen = set()
+    
 
-    while left < right:
-        width = right - left
-        current_height = min(nums[left],  nums[right])
-        area = width * current_height
-
-        max_area = max(area, max_area)
-
-        if nums[left] < nums[right]:
-            left += 1
-        else:
-            right -= 1
-
-    return max_area
-
-
-
-
-nums = [1,8,6,2,5,4,8,3,7]
-result = my_function(nums)
+s1 = "abcabcbb"
+result = my_funtion(s1)
 print(result)
-
